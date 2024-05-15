87fabb030d0a2f4afa838fed3977bef7872b61fc
olatejulian <julian.lf.olate@gmail.com>
2024-05-15 20:14:57 -0300

    refactor: :fire: return to api repo

--- react/01/src/UserGreeting.jsx
+++
@@ -1,9 +0,0 @@
-function UserGreeting(props) {
-    const logged = <h2>Welcome {props.username}</h2>;
-
-    const nonLogged = <h2>Meu pau no seu ouvido</h2>;
-
-    return props.isLoggedIn ? logged : nonLogged;
-}
-
-export default UserGreeting;
